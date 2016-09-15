347ece3def6d31accd3e136ff7d3b5937fe0306c
CodeMonkeyInit <deniskuliev@MacBook-Pro-Denis.local>
2016-09-15 14:01:22 +0300

    Initial Commit

---
+++ courseWork/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  courseWork
+//
+//  Created by Денис Кулиев on 15.09.16.
+//  Copyright © 2016 Денис Кулиев. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
